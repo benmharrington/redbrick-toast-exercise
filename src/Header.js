71b8df765873ba2a9a618a3acfb1464ff94af7cd
Ben Harrington <benmharrington@gmail.com>
2024-07-17 15:45:45 -0700

    Clicking 'new submission' adds a snackbar with new first name, last name, and email.

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
@@ -7,10 +7,70 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Snackbar from '@mui/material/Snackbar';
+import Slide from '@mui/material/Slide';
+import CloseIcon from '@mui/icons-material/Close';
 
-import { createMockFormSubmission } from './service/mockServer';
+import { createMockFormSubmission, onMessage } from './service/mockServer';
+import { Stack } from '@mui/material';
+
+const defaultSnackbarContent = {
+  id: null,
+  data: {
+    email: '',
+    firstName: '',
+    lastName: '',
+    liked: false,
+  },
+};
+
+function SlideTransition(props) {
+  return <Slide {...props} direction="up" />;
+}
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+  const [snackbarContent, setSnackbarContent] = useState(defaultSnackbarContent);
+
+  const createSnackbarContent = submissionData => {
+    setSnackbarContent({ ...submissionData });
+  }
+
+  const handleNewSubmission = () => {
+    createMockFormSubmission();
+    setOpen(true);
+  }
+
+  const handleLike = () => {
+    console.log('save content');
+  }
+
+  const handleClose = (_, reason) => {
+    if (reason === 'clickaway') return;
+
+    setOpen(false);
+  }
+
+  const snackbarActions = (
+    <React.Fragment>
+      <Button color="primary" size="small" onClick={handleLike}>
+        Like
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+
+  useEffect(() => {
+    onMessage(createSnackbarContent);
+  }, []);
+
   return (
     <Box sx={{flexGrow: 1}}>
       <AppBar position="static">
@@ -30,12 +90,24 @@ export default function Header() {
             variant="contained"
             size="small"
             color="secondary"
-            onClick={() => createMockFormSubmission()}
+            onClick={handleNewSubmission}
           >
             New Submission
           </Button>
         </Toolbar>
       </AppBar>
+      <Snackbar
+        open={open}
+        onClose={handleClose}
+        message={(
+          <Stack sx={{mr: 4}}>
+            <Typography>{snackbarContent?.data?.firstName} {snackbarContent?.data?.lastName}</Typography>
+            <Typography>{snackbarContent?.data?.email}</Typography>
+          </Stack>
+        )}
+        action={snackbarActions}
+        TransitionComponent={SlideTransition}
+      />
     </Box>
   );
 }
